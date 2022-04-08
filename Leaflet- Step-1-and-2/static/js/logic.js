5002eabfd62b559930c9f932cb52731930e97100
mikulr <94499909+mikulr@users.noreply.github.com>
2022-04-08 17:15:20 -0400

    ?

--- Leaflet- Step-1-and-2/static/js/logic.js
+++ Leaflet- Step-1-and-2/static/js/logic.js
@@ -2,127 +2,155 @@
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 let platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
-// Perform a GET request to the query URL fgor earthquakes
-d3.json(queryUrl).then(function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.features)
-});
-
-// Perform a GET request to the local file for plates
-// d3.json("data/plates.json").then(function (data) {
-//     // Once we get a response, send the data.features object to the createPlates function.
-//     createPlates(data.features)
-// });
-
-   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-   var myMap = L.map("map", {
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+let myMap = L.map("map", {
     center: [
         37.09, -95.71
     ],
-    zoom: 4,
-   
+    zoom: 3,
+    layers: []
 });
 
+// Define a function that we want to run once for each feature in the features array.
+// Give each feature a popup that describes the place and time of the earthquake.
+function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3>
+    <p>${new Date(feature.properties.time)}</p>
+    <p>Magnitude: ${(feature.properties.mag)}</p>
+    <p>Signifigance: ${(feature.properties.sig)}</p>
+    <p>Depth: ${(feature.geometry.coordinates[2])}</p>
+    `)
+};
 
-
-function createFeatures(earthquakeData) {
-    console.log("data:", earthquakeData);
-    // Define a function that we want to run once for each feature in the features array.
-    // Give each feature a popup that describes the place and time of the earthquake.
-
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3>
-        <p>${new Date(feature.properties.time)}</p>
-        <p>Magnitude: ${(feature.properties.mag)}</p>
-        <p>Signifigance: ${(feature.properties.sig)}</p>
-        <p>Depth: ${(feature.geometry.coordinates[2])}</p>
-        `)
-    };
-
-    // Assign color ranges that will be based on the signifigance scale below
-    function getColor(d) {
-        return d > 500 ? '#800026' :
-            d > 400 ? '#BD0026' :
-                d > 300 ? '#E31A1C' :
-                    d > 200 ? '#FC4E2A' :
-                        d > 150 ? '#FD8D3C' :
-                            d > 100 ? '#FEB24C' :
-                                d > 50? '#FED976' :
-                                    '#FFEDA0';
-    }
-    // set the style of each feature using values reurned from the geoJson
-    function styleFeature(feature, latlng) {
-        return L.circleMarker(latlng, {
-            fillOpacity: feature.geometry.coordinates[2] / 50,
-            color: getColor(feature.properties.sig),
-            fillColor: getColor(feature.properties.sig),
-            radius: Math.sqrt(feature.properties.sig)
-        });
-    }
-    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-    // Run the onEachFeature function once for each piece of data in the array.
-    var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature,
-        pointToLayer: styleFeature
-    });
-    
-    // Send our earthquakes layer to the createMap function/
-    createMap(earthquakes);
-}          
-
-
-
-// function createPlates(platesData) {
-//     console.log("platesData:", platesData);
-
-//     // var plates = L.geoJSON(platesData, {
-//     //     onEachFeature: 
-// };
-
-
-
-
-function createMap(earthquakes) {
-
-    //     // Create the base layers.
-    var topo = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/satellite-streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: API_KEY,
-    }).addTo(myMap)
-
-    var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
-        maxZoom: 20,
-        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
-    }).addTo(myMap)
-
-    var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: API_KEY,
-    }).addTo(myMap)
-
-    // Create a baseMaps object.
-    var baseMaps = {
-        "Street Map": streets,
-        "Satellite": topo,
-        "USGS": USGS_USImagery
-    };
-
-    // Create an overlay object to hold our overlay.
-    var overlayMaps = {
-        "Earthquakes": earthquakes
-    };
-
-     // Create a layer control.
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap)
-
+// Assign color ranges that will be based on the signifigance scale below
+function getColor(d) {
+    return d > 500 ? '#1b9e77' :
+        d > 400 ? '#d95f02' :
+            d > 300 ? '#7570b3' :
+                d > 200 ? '#e7298a' :
+                    d > 150 ? '#66a61e' :
+                        d > 100 ? '#e6ab02' :
+                            d > 50 ? '#a6761d' :
+                                '#666666';
 }
+
+// set the style of each feature using values reurned from the geoJson
+function styleFeature(feature, latlng) {
+    return L.circleMarker(latlng, {
+        fillOpacity: feature.geometry.coordinates[2] / 50,
+        color: getColor(feature.properties.sig),
+        fillColor: getColor(feature.properties.sig),
+        radius: Math.sqrt(feature.properties.sig)
+    });
+}
+
+
+
+let earthquakes = new L.LayerGroup()
+let plates = new L.LayerGroup()
+
+d3.json(queryUrl).then(function (data) {
+    console.log("earthquakedata:", data);
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    var quakes = L.geoJSON(data, {
+        // Run the onEachFeature function once for each piece of data in the array.
+        onEachFeature: onEachFeature,
+        // pointToLayer will style as defined in styleFeature
+        pointToLayer: styleFeature
+        // first add it to var created above- if you dont then you cant access the data to reference below
+    }).addTo(earthquakes)
+});
+// add layer from above (that you put the data in) to the map
+earthquakes.addTo(myMap);
+
+// Perform a GET request to the local file for plates
+d3.json(platesUrl).then(function (data) {
+    console.log("platesData:", data);
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    var plat = L.geoJSON(data, {
+        // Lines dont need much styling at least!
+        color: "yellow"
+        // first add it to var created above- if you dont then you cant access the data to reference below
+    }).addTo(plates)
+});
+// add layer from above (that you put the data in) to the map
+plates.addTo(myMap);
+
+
+
+// Create the base layers.
+// var topo = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/satellite-streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY,
+// }).addTo(myMap)
+
+var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 20,
+    attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+}).addTo(myMap)
+
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY,
+// }).addTo(myMap)
+
+// Create a baseMaps object.
+var baseMaps = {
+    "Street Map": street,
+    "Topographical": topo,
+    "USGS": USGS_USImagery
+};
+
+// Create an overlay object to hold our overlay.
+var overlayMaps = {
+    "Earthquakes": earthquakes,
+    "Tectonic Plates": plates
+};
+
+// Create a layer control.
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(myMap)
+
+// Add Legend
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        labels = ['<h4>Earthquake <br> Intensity</h4>'],
+        grades = [0, 50, 100, 150, 200, 300, 400, 500];
+
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            labels.push(
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'));
+    };
+    div.innerHTML = labels.join('<br>');
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+
+
